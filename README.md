2775745e01e01e0a1ca7050e725de28a0d5475ff
bernat <bernat@bestiario.org>
2025-04-04 14:21:23 +0200

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+- `python -m pip install https://software-download.bantamtools.com/nd/api/nextdraw_api.zip --upgrade --upgrade-strategy eager`
---
+++ utils.py
@@ -0,0 +1,28 @@
+# imports
+from pathlib import Path, PurePath
+import sys  
+
+# Get my_package directory path from Notebook
+parent_dir = str(PurePath(Path().resolve().parents[0], 'utils'))
+# Add to sys.path
+sys.path.insert(0, parent_dir)
+
+
+parent_dir = str(PurePath(Path().resolve().parents[1], 'utils'))
+# Add to sys.path
+sys.path.insert(0, parent_dir)
+
+
+# Get my_package directory path from Notebook
+parent_dir = str(PurePath(Path().resolve().parents[0], 'nextdraw_api'))
+# Add to sys.path
+sys.path.insert(0, parent_dir)
+
+
+parent_dir = str(PurePath(Path().resolve().parents[1], 'nextdraw_api'))
+# Add to sys.path
+sys.path.insert(0, parent_dir)
+
+
+import importlib
+reload = importlib.reload
