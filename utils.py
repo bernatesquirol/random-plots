927f3b2772b7b25494f892bf95e8b3eda62bc48d
bernat <bernatesquirol@gmail.com>
2025-04-04 19:29:12 +0200

    poca cosa

--- utils.py
+++ utils.py
@@ -12,17 +12,5 @@ parent_dir = str(PurePath(Path().resolve().parents[1], 'utils'))
 # Add to sys.path
 sys.path.insert(0, parent_dir)
 
-
-# Get my_package directory path from Notebook
-parent_dir = str(PurePath(Path().resolve().parents[0], 'nextdraw_api'))
-# Add to sys.path
-sys.path.insert(0, parent_dir)
-
-
-parent_dir = str(PurePath(Path().resolve().parents[1], 'nextdraw_api'))
-# Add to sys.path
-sys.path.insert(0, parent_dir)
-
-
 import importlib
 reload = importlib.reload
